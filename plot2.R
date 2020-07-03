d7ea0004ed1668b14f628d120517d30c5e7559d2
gita-liferishi <64720346+gita-liferishi@users.noreply.github.com>
2020-07-03 09:14:43 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,21 @@
+##downloading & extracting file
+library(sqldf)
+
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+hpc <- "power_cons.zip"
+download.file(url, hpc)
+unzip("power_cons.zip")
+
+
+readHPC <- read.csv.sql("household_power_consumption.txt", sep = ";", sql = 'select * from file where Date IN ("1/2/2007", "2/2/2007")')
+readHPC$Date <- as.Date(readHPC$Date, "%m/%d/%Y")
+dt <- paste(household_data$Date, household_data$Time)
+readHPC$date_time <- strptime(dt, "%Y-%d-%m %H:%M:%S")
+
+Sys.setlocale("LC_TIME", "C") 
+
+with(household_data, plot(Time_new, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+dev.copy(png, file = "plot2.png", width = 480, height = 480)
+
+dev.off()
